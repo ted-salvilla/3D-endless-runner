eb226b75d0ea89bf14b039fd7bee38604c37dd8f
ted-salvilla <61590533+ted-salvilla@users.noreply.github.com>
2020-03-04 11:29:45 +0800

    3D endless runner
    3D endless runner

---
+++ Assets/Scripts/GenerateWorld.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    public GameObject[] platforms;
+    GameObject dummyTraveller;
+
+    void Start ()
+    {
+        dummyTraveller = new GameObject("dummy");
+        
+        for(int i =0; i < 20; i++)
+        {
+            int platformNumber = Random.Range(0, platforms.Length);
+            GameObject p = Instantiate(platforms[platformNumber],
+                                    dummyTraveller.transform.position,
+                                    dummyTraveller.transform.rotation);
+
+
+            if (platforms[platformNumber].tag == "stairsUp")
+            {
+                dummyTraveller.transform.Translate(0, 5, 0);
+            }
+
+            else if (platforms[platformNumber].tag == "stairsDown")
+            {
+                dummyTraveller.transform.Translate(0, -5, 0);
+                p.transform.Rotate(new Vector3(0, 180, 0));
+                p.transform.position = dummyTraveller.transform.position;
+            }
+            else if (platforms[platformNumber].tag == "platformTSection")
+            {
+                if (Random.Range (0,2) == 0)
+                    dummyTraveller.transform.Rotate(new Vector3(0, 90, 0));
+                else
+                    dummyTraveller.transform.Rotate(new Vector3(0, -90, 0));
+                    dummyTraveller.transform.Translate(Vector3.forward * -10);
+            }
+            dummyTraveller.transform.Translate(Vector3.forward * -10);
+        }
+
+    }
+}
